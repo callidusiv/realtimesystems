e46cbf9c825bea715e7f7177c17fc01c4b1deb30
Bohdan Ivanishchev <callidus.iv@gmail.com>
2017-05-16 12:45:28 +0300

    Initial Commit

---
+++ src/main/java/ua/com/proximus/realtimesystems/RealTimeSystems.java
@@ -0,0 +1,8 @@
+package ua.com.proximus.realtimesystems;
+
+
+public class RealTimeSystems {
+  public static void main(String[] args) {
+    System.out.println("RealTimeSystems app start!");
+  }
+}
